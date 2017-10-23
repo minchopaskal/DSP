0cc0fdacdf7891f244f0d61a4070785406b60f9c
Mincho Paskalev <minchopaskal@gmail.com>
2017-10-23 19:55:42 +0300

    main to show off the priority queue implementation

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "PriorityQueue.h"
+
+struct StringComparison {
+   bool operator() (std::string fstString, std::string sndString) const {
+       return fstString.compare(sndString) <= 0;
+   }
+};
+
+int main() {
+    PriorityQueue<char, std::string, StringComparison> myQueue;
+
+    myQueue.push('a', "apple");
+    myQueue.push('b', "bapple");
+    myQueue.push('c', "syrup");
+    myQueue.push('d', "maple");
+
+    myQueue.print();
+    return 0;
+}
