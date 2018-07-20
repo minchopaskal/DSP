4d4baf5d11cc6f5687cf986848006a3b98f56398
Mincho <minchopaskal@gmail.com>
2018-07-20 15:35:08 +0300

    fixed get_left(), added remove logic without fixup logic

--- DS_Implementations/RedBlackTree/main.cpp
+++ DS_Implementations/RedBlackTree/main.cpp
@@ -6,11 +6,16 @@ using rbti = red_black_tree<int>;
 
 int main() {
   rbti t;
-  std::cout << sizeof(rbti::node) << std::endl;
-  t.insert(5);
-  t.insert(6);
-  t.insert(2);
+
   t.insert(8);
-  
+  t.insert(10);
+  t.insert(37);
+  t.insert(2);
+  t.insert(1);
+  t.insert(90);
+  t.insert(56);
+  t.insert(98);
+  t.insert(23);
+
   t.print();
 }
